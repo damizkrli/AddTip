65565bdb74fc93b913193f65a67ac9568bf80d6a
damizkrli <karli.damien@gmail.com>
2022-07-13 09:54:16 +0200

    installation de TailwindCss avec PostCss + mise en place de PurgeCss pour la prod

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ['./templates/**/*.html.twig'],
+    theme: {},
+    variants: {
+        extend: {},
+    },
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {}
+    }
+}
